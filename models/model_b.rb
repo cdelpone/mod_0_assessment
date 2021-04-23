3e8c24e839a81ac90e97efd87434b5a4592814cf
Christina Delpone <cdelpone@gmail.com>
2021-04-23 13:33:01 -0600

    Add pet model using hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+pet = {
+"breed" => 'yellow lab',
+"name" => 'Pesto',
+"age_in_dog_years" => 7,
+"weight_in_lbs" => 52.2,
+"spayed" => false,
+"neutered" => false,
+"fav_toys" => ['pheasant', 'lacrosse ball', 'dino nyla bone'],
+"new_cues" => ['rest', 'touch', 'under']
+}
