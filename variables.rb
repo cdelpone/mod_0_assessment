1a3b5ff3cd406f1080fe59146f4e19922c42d1d5
Christina Delpone <cdelpone@gmail.com>
2021-04-23 13:17:19 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,12 @@
+famous_person = "Alice Cooper"
+is_active = false
+famous_person = "Christina Marie"
+num_3 = 3
+num_9 = 9
+sum = num_3 + num_9
+animals = ["lion", "tiger", "bear"]
+
+animals.shift()
+puts animals # testing my code 
+
+puts famous_person.length
