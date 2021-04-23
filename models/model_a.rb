bbb61081981fbf3e093ee60cca10344b49f5d8e7
Christina Delpone <cdelpone@gmail.com>
2021-04-23 13:28:13 -0600

    Add school model using hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+"grade_level" => 'K-12',
+"name" => 'Big School High',
+"num_of_classrooms" => 20,
+"num_of_teachers" => 25,
+"accredited" => true,
+"code_class" => false,
+"extracurriculars" => ['art class', 'workshop', 'culinary'],
+"cafeteria_food" => ['hamburgers', 'chicken nuggets', 'apple sauce']
+}
